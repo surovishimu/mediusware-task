fa403437f6d5fd806ed887209f46843831db9986
surovishimu <surovi.shimu95@gmail.com>
2023-12-30 04:07:25 +0600

    add checkbox and short by even id

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 
 const Problem2 = () => {
@@ -7,6 +7,7 @@ const Problem2 = () => {
     const [modalCIsOpen, setModalCIsOpen] = useState(false);
     const [contacts, setContacts] = useState([]);
     const [selectedContact, setSelectedContact] = useState(null);
+    const [showEvenContacts, setShowEvenContacts] = useState(false);
 
     const openModalA = async (url) => {
         try {
@@ -14,6 +15,10 @@ const Problem2 = () => {
             const data = await response.json();
             setContacts(data.results);
             setModalAIsOpen(true);
+
+          
+            const newUrl = window.location.origin + window.location.pathname + '?modal=a';
+            window.history.pushState({ modal: 'a' }, '', newUrl);
         } catch (error) {
             console.error('Error fetching contacts:', error);
         }
@@ -25,6 +30,10 @@ const Problem2 = () => {
             const data = await response.json();
             setContacts(data.results);
             setModalBIsOpen(true);
+
+         
+            const newUrl = window.location.origin + window.location.pathname + '?modal=b';
+            window.history.pushState({ modal: 'b' }, '', newUrl);
         } catch (error) {
             console.error('Error fetching contacts:', error);
         }
@@ -41,6 +50,22 @@ const Problem2 = () => {
         setModalCIsOpen(false);
         setSelectedContact(null);
     };
+
+    const handleCheckboxChange = () => {
+        setShowEvenContacts(!showEvenContacts);
+    };
+
+    useEffect(() => {
+     
+        const urlParams = new URLSearchParams(window.location.search);
+        const modalType = urlParams.get('modal');
+
+        if (modalType === 'a') {
+            openModalA('https://contact.mediusware.com/api/contacts/?page=1&page_size=120');
+        } else if (modalType === 'b') {
+            openModalB('https://contact.mediusware.com/api/country-contacts/united%20states/');
+        }
+    }, []);
 
     return (
         <div className="container">
@@ -70,13 +95,17 @@ const Problem2 = () => {
 
                 <Modal isOpen={modalAIsOpen || modalBIsOpen} onRequestClose={closeModal}>
                     <h2>Contact Modal</h2>
+
                     <ul>
-                        {contacts.map((contact) => (
-                            <li key={contact.id} onClick={() => openModalC(contact)}>
-                                {contact.phone}
-                            </li>
-                        ))}
+                        {contacts
+                            .filter((contact, index) => (!showEvenContacts || index % 2 === 0))
+                            .map((contact) => (
+                                <li key={contact.id} onClick={() => openModalC(contact)}>
+                                    {contact.phone}
+                                </li>
+                            ))}
                     </ul>
+
                     <button
                         className="btn btn-primary"
                         style={{ backgroundColor: '#46139f' }}
@@ -87,7 +116,9 @@ const Problem2 = () => {
                     <button
                         className="btn btn-warning"
                         style={{ backgroundColor: '#ff7f50' }}
-                        onClick={() => openModalB('https://contact.mediusware.com/api/country-contacts/united%20states/')}
+                        onClick={() =>
+                            openModalB('https://contact.mediusware.com/api/country-contacts/united%20states/')
+                        }
                     >
                         US Contacts
                     </button>
@@ -98,6 +129,16 @@ const Problem2 = () => {
                     >
                         Close
                     </button>
+                    <div>
+                        <label>
+                            Only Even
+                            <input
+                                type="checkbox"
+                                checked={showEvenContacts}
+                                onChange={handleCheckboxChange}
+                            />
+                        </label>
+                    </div>
                 </Modal>
 
                 <Modal isOpen={modalCIsOpen} onRequestClose={closeModal}>
@@ -105,20 +146,23 @@ const Problem2 = () => {
                     {selectedContact && (
                         <div>
                             <p>Name: {selectedContact.country.name}</p>
-                           
                         </div>
                     )}
                     <button
                         className="btn btn-primary "
                         style={{ backgroundColor: '#46139f' }}
-                        onClick={() => openModalB('https://contact.mediusware.com/api/country-contacts/united%20states/')}
+                        onClick={() =>
+                            openModalA('https://contact.mediusware.com/api/contacts/?page=1&page_size=120')
+                        }
                     >
                         All Contacts
                     </button>
                     <button
                         className="btn btn-warning"
                         style={{ backgroundColor: '#ff7f50' }}
-                        onClick={() => openModalB('https://contact.mediusware.com/api/country-contacts/united%20states/')}
+                        onClick={() =>
+                            openModalB('https://contact.mediusware.com/api/country-contacts/united%20states/')
+                        }
                     >
                         US Contacts
                     </button>
